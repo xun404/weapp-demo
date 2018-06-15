0a1a8c1764c979de543f5b24db025cf8cc170ba3
xun <mx@rmbz.net>
2018-06-15 17:02:19 +0800

    2018.06.14更新,基础库 2.1.0,v22

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
 # weapp-demo
 微信小程序官方组件展示(持续更新)
 
+- 2018.06.14 v23 基础库更新（2.1.0）
+    A 新增 API wx.loadFontFace 支持动态加载字体 详情
+    A 新增 API wx.getLogManager 可记录用户操作日志，供用户提交意见反馈内容时，提交给开发者协助定位问题 详情
+    A 新增 组件 <camera /> 的 scanCode 模式，支持识别一维码 详情
+    U 更新 API wx.createSelectorQuery 可获取节点的样式信息 详情
+    U 更新 API wx.scanCode 返回参数新增 rawData 字段 详情
+    U 更新 API wx.createLivePusher 新增 toggleTorch 方法支持切换闪光灯 详情
+    U 更新 组件 <button /> 可以嵌套在原生组件内，显示在原生组件之上 详情
+    U 更新 组件 <button /> 新增 open-type 值为 feedback，打开“意见反馈”页面 详情
+    U 更新 组件 <cover-view /> <cover-image /> 新增 scroll-top 属性 详情
+    U 更新 组件 <cover-view /> <cover-image /> 支持 rotate 和 scale 的样式设置 详情
+    U 更新 组件 <cover-image /> 新增 bindload 和 binderror 事件 详情
+    U 更新 组件 <movable-view /> 新增 animation 属性 详情
+    U 更新 组件 <live-pusher /> 新增 zoom 属性支持调整焦距 详情
+    U 更新 组件 <input /> 在 bindinput 时返回 keyCode 键值 详情
+    F 修复 API wx.reportAnalytics 在插件下调用异常的问题
+    F 修复 API wx.hideToast 在 iOS 下无法让前面的 showLoading 消失的问题
+    F 修复 组件 <image /> 在连续更新src时导致显示不正确的问题
+    F 修复 组件 <image /> 设置 border-radius 出现闪动的问题
+    F 修复 组件 <canvas /> 设置的shadow会影响fillStyle的问题
+    F 修复 组件 <movable-view /> 缩放的问题 详情
+    F 修复 组件 <input /> 在某些情况下未被正常销毁的问题 详情
+    F 修复 组件 <video /> 设置 playbackRate 为 1.25 无效的问题
+    F 修复 组件 <map /> 在开发者工具双击地图缩放不触发 bindregionchange 的问题 详情
+    F 修复 框架 在 video 全屏后有setData操作时导致字体变大的问题
+    
 - 2018.05.21 v22 更新
 
--- page/API/pages/web-socket/web-socket.js
+++ page/API/pages/web-socket/web-socket.js
@@ -1,12 +1,12 @@
-function t(t, o) {
+function t(t, e) {
     wx.showModal({
         title: t,
-        content: o,
+        content: e,
         showCancel: !1
     });
 }
 
-function o(t) {
+function e(t) {
     wx.showToast({
         title: t,
         icon: "success",
@@ -14,65 +14,63 @@ function o(t) {
     });
 }
 
-var e = require("../../../../vendor/qcloud-weapp-client-sdk/index"), s = require("../../../../config").loginUrl, n = require("../../../../config").tunnelUrl;
+require("../../../../vendor/qcloud-weapp-client-sdk/index"), require("../../../../config").loginUrl, 
+require("../../../../config").tunnelUrl;
 
 Page({
     data: {
         socketStatus: "closed"
     },
     onLoad: function() {
-        var t = this;
-        e.setLoginUrl(s), e.login({
-            success: function(o) {
-                console.log("登录成功", o), t.setData({
-                    hasLogin: !0
-                });
-            },
-            fail: function(t) {
-                console.log("登录失败", t);
-            }
+        this.setData({
+            hasLogin: !0
         });
     },
     onUnload: function() {
         this.closeSocket();
     },
     toggleSocket: function(t) {
-        var o = t.detail.value;
-        if (o && "closed" == this.data.socketStatus) this.openSocket(); else if (!o && "connected" == this.data.socketStatus) {
+        var e = t.detail.value;
+        if (e && "closed" == this.data.socketStatus) this.openSocket(); else if (!e && "connected" == this.data.socketStatus) {
             this.closeSocket(!0);
         }
     },
     openSocket: function() {
-        var s = this, c = this.socket = new e.Tunnel(n);
-        c.on("connect", function() {
-            console.log("WebSocket 已连接"), o("Socket已连接"), s.setData({
+        var o = this;
+        wx.onSocketOpen(function() {
+            console.log("WebSocket 已连接"), e("Socket已连接"), o.setData({
                 socketStatus: "connected",
                 waitingResponse: !1
             });
-        }), c.on("close", function() {
-            console.log("WebSocket 已断开"), s.setData({
+        }), wx.onSocketClose(function() {
+            console.log("WebSocket 已断开"), o.setData({
                 socketStatus: "closed"
             });
-        }), c.on("error", function(o) {
-            t("发生错误", JSON.stringify(o)), console.error("socket error:", o), s.setData({
+        }), wx.onSocketError(function(e) {
+            t("发生错误", JSON.stringify(e)), console.error("socket error:", e), o.setData({
                 loading: !1
             });
-        }), c.on("message", function(t) {
-            o("收到信道消息"), console.log("socket message:", t), s.setData({
+        }), wx.onSocketMessage(function(t) {
+            e("收到信道消息"), console.log("socket message:", t), o.setData({
                 loading: !1
             });
-        }), c.open();
+        }), wx.connectSocket({
+            url: "wss://echo.websocket.org"
+        });
     },
     closeSocket: function(t) {
-        this.socket && this.socket.close(), t && o("Socket已断开"), this.setData({
-            socketStatus: "closed"
+        var o = this;
+        "connected" === this.data.socketStatus && wx.closeSocket({
+            success: function() {
+                e("Socket已断开"), o.setData({
+                    socketStatus: "closed"
+                });
+            }
         });
     },
     sendMessage: function() {
-        this.socket && this.socket.isActive() && (this.socket.emit("message", {
-            content: "Hello, 小程序!"
-        }), this.setData({
-            loading: !0
-        }));
+        "connected" === this.data.socketStatus && wx.sendSocketMessage({
+            data: "Hello, Miniprogram!"
+        });
     }
 });
